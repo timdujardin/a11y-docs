94016ea70efda15de16545b77becad603b5b620d
Tim Dujardin <tim.dujardin@iodigital.com>
2024-08-07 11:48:27 +0200

    Updated astro config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://timdujardin.github.io",
+  base: "a11y-docs",
   integrations: [
     starlight({
       title: "A11Y Docs",
